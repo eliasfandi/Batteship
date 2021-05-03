39a288b38e301e718ab008a69b1c29c7d8381130
Elias Fandi <16519982+elifandi@users.noreply.github.com>
2021-05-03 14:42:05 +0200

    Update CliGame.java

--- src/trafficlights/CliGame.java
+++ src/trafficlights/CliGame.java
@@ -19,7 +19,15 @@ public class CliGame {
             System.out.println("Enter where you want to aim: ");
             String position = input.nextLine();
 
-            input.close();
+            //input.close();
+
+            //0 ocean 1 miss 2 ship hit 3 ship present
+            int y = (int) position.charAt(0);
+            int x = (int) position.charAt(1);
+            y = (y - 64);
+            int hitLoc = (y*10) + x;
+
+            model.registerHit(hitLoc);
 
         }
     }
