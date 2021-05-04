a3e04478c844a9fb3d30d08b460b9399ac16481e
Elias Fandi <16519982+elifandi@users.noreply.github.com>
2021-05-04 19:17:47 +0200

    Create Controller.java

---
+++ src/trafficlights/Controller.java
@@ -0,0 +1,16 @@
+package trafficlights;
+
+import com.sun.org.apache.xpath.internal.operations.Mod;
+
+public class Controller
+
+{
+    private Model model;
+    private View view;
+
+    public Controller(Model  model) {
+        this.model = model;
+
+    }
+
+}
