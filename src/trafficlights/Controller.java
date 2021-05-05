28191b19be0cb74355e0eaf2190fd42e3511b24b
Elias Fandi <16519982+elifandi@users.noreply.github.com>
2021-05-05 16:25:00 +0200

    Update Controller.java

--- src/trafficlights/Controller.java
+++ src/trafficlights/Controller.java
@@ -2,6 +2,8 @@ package trafficlights;
 
 import com.sun.org.apache.xpath.internal.operations.Mod;
 
+import java.util.Map;
+
 public class Controller
 
 {
@@ -11,6 +13,22 @@ public class Controller
     public Controller(Model  model) {
         this.model = model;
 
+    }
+    public void registerHit(int location) {
+        model.registerHit(location);
+    }
+
+    public int getTileStatus (int location) {
+        Map<Integer, Integer> map = model.getShipMap();
+        int tileStatus;
+        if (map.containsKey(location)) {
+            tileStatus = map.get(location);
+
+        }
+        else{
+            tileStatus = 0;
+        }
+        return tileStatus;
     }
 
 }
